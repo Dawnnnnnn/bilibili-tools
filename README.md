60506b5b745f3ce9c11b2e21ac4e3df1126c063d
Dawnnnnnn <1050596704@qq.com>
2019-02-22 09:59:49 +0800

    删除投票功能

--- README.md
+++ README.md
@@ -2,32 +2,29 @@
 
 挖坑开始
 
-此项目为bilibili主站各项功能实现
+此项目为 bilibili 主站各项功能实现
 
-另一已完成项目为bilibili直播站各功能实现 https://github.com/Dawnnnnnn/bilibili-live-tools
-
-
+另一已完成项目为 bilibili 直播站各功能实现 https://github.com/Dawnnnnnn/bilibili-live-tools
 
 目前已实现:
 
-    风纪委员投票
+    <!-- 风纪委员投票 -->
     每日投币
     每日登录
     每日分享
     每日观看
 
-
-
 Q:观看，分享，投币的视频如何选取?
 
-A:拉取关注列表，并在关注列表里up主投稿中随机选几个视频
+A:拉取关注列表，并在关注列表里 up 主投稿中随机选几个视频
 
 Q:如何关闭自己不想要的功能?
 
 A:
+
 >
 
-    tasks2 = [ 
+    tasks2 = [
         # judge().coin_run(),   # 投币任务
         judge().share_run(),  # 分享任务
         judge().watch_run(),  # 观看任务
@@ -37,4 +34,6 @@ A:
 
 默认不打开投币功能，如想快速提高主站等级，把那一行注释去掉就可以了
 
-挖坑结束，后期完善一下issue里提到的案件仲裁问题就没事了。
+挖坑结束，后期完善一下 issue 里提到的案件仲裁问题就没事了。
+
+懒的加那个投票阙值了。。。。案件仲裁直接删了....还想用的话就用上一个版本。。
--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def CurrentTime():
     currenttime = str(int(time.mktime(datetime.datetime.now().timetuple())))
     return currenttime
 
+
 class login():
     cookies = ""
     username = input("输入用户名:")
@@ -29,7 +30,7 @@ class login():
     uid = ""
     access_key = ""
 
-    async def calc_sign(self,str):
+    async def calc_sign(self, str):
         str = str + "560c52ccd288fed045859ed18bffd973"
         hash = hashlib.md5()
         hash.update(str.encode('utf-8'))
@@ -46,7 +47,8 @@ class login():
         key = value['key']
         Hash = str(value['hash'])
         pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(key.encode())
-        password = base64.b64encode(rsa.encrypt((Hash + password).encode('utf-8'), pubkey))
+        password = base64.b64encode(rsa.encrypt(
+            (Hash + password).encode('utf-8'), pubkey))
         password = parse.quote_plus(password)
         username = parse.quote_plus(username)
         return username, password
@@ -63,7 +65,8 @@ class login():
             cookie = (response.json()['data']['cookie_info']['cookies'])
             cookie_format = ""
             for i in range(0, len(cookie)):
-                cookie_format = cookie_format + cookie[i]['name'] + "=" + cookie[i]['value'] + ";"
+                cookie_format = cookie_format + \
+                    cookie[i]['name'] + "=" + cookie[i]['value'] + ";"
             s1 = re.findall(r'bili_jct=(\S+)', cookie_format, re.M)
             s2 = re.findall(r'DedeUserID=(\S+)', cookie_format, re.M)
             login.cookies = cookie_format
@@ -75,7 +78,8 @@ class login():
             }
             login.csrf = (s1[0]).split(";")[0]
             login.uid = (s2[0].split(";")[0])
-            login.access_key = response.json()['data']['token_info']['access_token']
+            login.access_key = response.json(
+            )['data']['token_info']['access_token']
         except:
             print("登录失败，回显为:", response.json())
 
@@ -89,97 +93,24 @@ class judge(login):
         millis = int((time.time() * 1000))
         return str(millis)
 
-    async def caseObtain(self):
-        try:
-            url = 'http://api.bilibili.com/x/credit/jury/caseObtain'
-            data = {'jsonp':'jsonp','csrf':login.csrf}
-            response = requests.post(url, headers=login.headers,data=data)
-            temp = response.json()
-            print(temp)
-            if temp['code'] == 0:
-                id = temp['data']['id']
-                return id
-            if temp['code'] == -101:
-                print("cookie失效,尝试重新登录!")
-                await self.login()
-                await self.judge()
-            if temp['code'] == 25014 or 25008:
-                print("今日的案件已经审理完毕了噢~,休眠六小时后会再次开始审理")
-                await asyncio.sleep(21600)
-                await self.judge()
-        except:
-            print("caseObtain模块出错")
-            await self.judge()
-
-    async def check(self):
-        vote = 4
-        id = await self.caseObtain()
-        headers = {
-            "Host": "api.bilibili.com",
-            "Referer": "https://www.bilibili.com/judgement/vote/" + str(id),
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36",
-            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-            "Cookie": login.cookies
-        }
-        try:
-            url = "https://api.bilibili.com/x/credit/jury/juryCase?jsonp=jsonp&callback=jQuery1720" + str(
-                self.randomint()) + "_" + self.CurrentTime() + "&cid=" + str(id) + "&_=" + self.CurrentTime()
-            response = requests.get(url, headers=headers)
-            print(response.text)
-            temp = (response.text[42:-1])
-            temp = json.loads(temp)
-            print(temp)
-            votebreak = temp['data']['voteBreak']
-            voteDelete = temp['data']['voteDelete']
-            voteRule = temp['data']['voteRule']
-            if votebreak > voteDelete:
-                vote = 1
-            if votebreak < voteDelete:
-                vote = 4
-            if (voteRule > votebreak) and (voteRule > voteDelete):
-                vote = 2
-            return id, vote
-        except:
-            print("check模块出错")
-            await asyncio.sleep(120)
-            await self.judge()
-
-    async def judge(self):
-        try:
-            id, vote = await self.check()
-            url = 'http://api.bilibili.com/x/credit/jury/vote'
-            payload = {
-                "jsonp": "jsonp",
-                "cid": id,
-                "vote": vote,
-                "content": "",
-                "likes": "",
-                "hates": "",
-                "attr": "1",
-                "csrf": login.csrf
-            }
-            response = requests.post(url, headers=login.headers, data=payload)
-            print(response.json())
-        except:
-            print('judge出错')
-
     async def query_reward(self):
         url = "https://account.bilibili.com/home/reward"
         headers = {
             "Referer": "https://account.bilibili.com/account/home",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
-            "Cookie":login.cookies
+            "Cookie": login.cookies
         }
-        response = requests.get(url,headers=headers)
+        response = requests.get(url, headers=headers)
         iflogin = response.json()['data']['login']
         ifwatch_av = response.json()['data']['watch_av']
         ifshare_av = response.json()['data']['share_av']
         ifgive_coin = response.json()['data']['coins_av']
-        return [iflogin,ifwatch_av,ifshare_av,int(ifgive_coin)]
+        return [iflogin, ifwatch_av, ifshare_av, int(ifgive_coin)]
 
     async def get_attention(self):
         top50_attention_list = []
-        url = "https://api.bilibili.com/x/relation/followings?vmid=" + str(login.uid) + "&ps=50&order=desc"
+        url = "https://api.bilibili.com/x/relation/followings?vmid=" + \
+            str(login.uid) + "&ps=50&order=desc"
         headers = {
             "Host": "api.bilibili.com",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36",
@@ -197,7 +128,8 @@ class judge(login):
         top50_attention_list = await self.get_attention()
         video_list = []
         for mid in top50_attention_list:
-            url = "https://space.bilibili.com/ajax/member/getSubmitVideos?mid=" + str(mid) + "&pagesize=100&tid=0"
+            url = "https://space.bilibili.com/ajax/member/getSubmitVideos?mid=" + \
+                str(mid) + "&pagesize=100&tid=0"
             response = requests.get(url)
             datalen = len(response.json()['data']['vlist'])
             for i in range(0, datalen):
@@ -223,13 +155,13 @@ class judge(login):
             "Cookie": login.cookies
         }
         response = requests.post(url, data=data, headers=headers)
-        print("coin_task:",response.text)
+        print("coin_task:", response.text)
 
         if response.json()['code'] != 0:
             await self.givecoin()
         await asyncio.sleep(10)
 
-    async def get_cid(self,aid):
+    async def get_cid(self, aid):
         url = "https://www.bilibili.com/widget/getPageList?aid="+str(aid)
         response = requests.get(url)
         cid = response.json()[0]['cid']
@@ -245,55 +177,47 @@ class judge(login):
             "Cookie": "sid=8wfvu7i7"
         }
         ts = CurrentTime()
-        temp_params = "access_key="+login.access_key+"&aid="+str(aid)+"&appkey=1d8b6e7d45233436&build=5260003&from=7&mobi_app=android&platform=android&ts="+str(ts)
+        temp_params = "access_key="+login.access_key+"&aid=" + \
+            str(aid)+"&appkey=1d8b6e7d45233436&build=5260003&from=7&mobi_app=android&platform=android&ts="+str(ts)
         sign = await self.calc_sign(temp_params)
         data = {
-            "access_key":login.access_key,
-            "aid":aid,
-            "appkey":"1d8b6e7d45233436",
-            "build":"5260003",
-            "from":"7",
-            "mobi_app":"android",
-            "platform":"android",
-            "ts":ts,
-            "sign":sign
+            "access_key": login.access_key,
+            "aid": aid,
+            "appkey": "1d8b6e7d45233436",
+            "build": "5260003",
+            "from": "7",
+            "mobi_app": "android",
+            "platform": "android",
+            "ts": ts,
+            "sign": sign
         }
-        response = requests.post(url1,headers=headers,data=data)
-        print("分享视频:",response.json())
+        response = requests.post(url1, headers=headers, data=data)
+        print("分享视频:", response.json())
 
-
-    async def watch_av(self,aid,cid):
+    async def watch_av(self, aid, cid):
         url = "https://api.bilibili.com/x/report/web/heartbeat"
         headers = {
             "Host": "api.bilibili.com",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
             "Referer": "https://www.bilibili.com/video/av"+str(aid),
-            "Cookie":login.cookies
+            "Cookie": login.cookies
         }
         data = {
-            "aid":aid,
-            "cid":cid,
-            "mid":login.uid,
-            "csrf":login.csrf,
-            "played_time":"0",
-            "realtime":"0",
-            "start_ts":self.CurrentTime(),
-            "type":"3",
-            "dt":"2",
-            "play_type":"1"
+            "aid": aid,
+            "cid": cid,
+            "mid": login.uid,
+            "csrf": login.csrf,
+            "played_time": "0",
+            "realtime": "0",
+            "start_ts": self.CurrentTime(),
+            "type": "3",
+            "dt": "2",
+            "play_type": "1"
         }
 
-        response = requests.post(url,headers=headers,data=data)
+        response = requests.post(url, headers=headers, data=data)
 
-        print("watch_Av_state:",response.text)
-
-    async def judge_run(self):
-        while 1:
-            try:
-                await self.judge()
-                await asyncio.sleep(random.randint(12,43))
-            except:
-                print('judge_run出错')
+        print("watch_Av_state:", response.text)
 
     async def coin_run(self):
         while 1:
@@ -325,11 +249,10 @@ class judge(login):
                 video_list = await self.getsubmit_video()
                 aid = video_list[random.randint(0, len(video_list))]
                 cid = await self.get_cid(aid)
-                await self.watch_av(aid,cid)
+                await self.watch_av(aid, cid)
                 await asyncio.sleep(21600)
             except:
                 print("watch_run出错")
-
 
 
 loop = asyncio.get_event_loop()
@@ -344,7 +267,6 @@ tasks2 = [
     judge().coin_run(),
     judge().share_run(),
     judge().watch_run(),
-    judge().judge_run()
 
 ]
 loop.run_until_complete(asyncio.wait(tasks2))
